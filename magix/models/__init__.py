2462dd5a19471c49cf2a0c48b729b45a9dfff580
Luyu Gao <gao1@dt-login01.delta.ncsa.illinois.edu>
2024-02-28 06:52:03 -0600

    [model] add gemma

--- magix/models/__init__.py
+++ magix/models/__init__.py
@@ -2,15 +2,18 @@ from .llama_model import FlaxLlamaModel, FlaxLlamaForCausalLM
 from .mistral_model import FlaxMistralModel, FlaxMistralForCausalLM
 from .bert_model import FlaxBertModel
 from .t5_model import FlaxT5EncoderModel
+from .gemma_model import FlaxGemmaModel, FlaxGemmaForCausalLM
 
 ENCODER_MODEL_MAPPING = {
     "llama": FlaxLlamaModel,
     "mistral": FlaxMistralModel,
     "bert": FlaxBertModel,
     "t5": FlaxT5EncoderModel,
+    "gemma": FlaxGemmaModel,
 }
 
 CAUSAL_LM_MODEL_MAPPING = {
     "llama": FlaxLlamaForCausalLM,
     "mistral": FlaxMistralForCausalLM,
+    "gemma": FlaxGemmaForCausalLM,
 }
